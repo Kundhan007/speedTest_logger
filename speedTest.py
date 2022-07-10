e59511a6bf3972b5fc902f922454b7c4cf55c47d
kundhan <kundhan1997@gmail.com>
2022-07-10 16:18:13 +0530

    working speed test with error case

--- speedTest.py
+++ speedTest.py
@@ -2,31 +2,27 @@
 import math
 
 import speedtest
+import subprocess
+
 def modifier(numbr):
     # rounding the number
     ten6 = math.pow(10, 6)
-    mb =  numbr/ten6
+    mb = numbr / ten6
     rnumb = round(mb)
     return rnumb
 
-def return_on_failure(value):
-  def decorate(f):
-    def applicator(*args, **kwargs):
-      try:
-         return f(*args,**kwargs)
-      except:
-         print('some issue with internet')
-         return value
 
 def test():
+    try:
+        return testWrapper()
+    except:
+        print('some internet issue')
+        return 0,0, 'not connected'
+
+def testWrapper():
     wifi = speedtest.Speedtest()
     down = modifier(wifi.download())
     up = modifier(wifi.upload())
     print("wifi down and up speed ", down, up)
     return down, up
-
-
-
-
-
 
